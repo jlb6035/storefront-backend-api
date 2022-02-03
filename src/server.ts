2b1d521be71856295c930bf07f42c9375f0ac8c7
jlb6035 <jlb6035@gmail.com>
2022-02-02 22:01:29 -0500

    updated port and package.json

--- src/server.ts
+++ src/server.ts
@@ -6,7 +6,7 @@ import orderRoutes from "./handlers/order";
 import cors from "cors";
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors())
 app.use(bodyParser.json())
